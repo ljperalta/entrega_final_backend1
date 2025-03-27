692aaaa3f8fb509104b8ad11dc19bfd0378be00b
ljperalta <leoperalta.lp24@gmail.com>
2025-03-27 00:42:54 -0300

    paginacion

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -5,26 +5,45 @@ const products = require('../managers/products');
 
 router.get('/', async (req, res) => {
     try {
-        const { page = 1, limit = 10 } = req.query; // Página actual y límite de resultados por página
-        //! Mostrar el paso a paso de como Implementar el paginate con mongoose-paginate-v2
-        //! Ir al Model student.js para ver el plugin mongoosePaginate
+        const { page = 1, limit = 10, sort, query, category, available } = req.query;
+        let filter = {};
         const options = {
-          page: parseInt(page),
-          limit: parseInt(limit),
-          sort: { grade: -1 },
+            page: parseInt(page),
+            limit: parseInt(limit),
+            sort: sort ? { price: sort === "asc" ? 1 : -1 } : {},
         };
+
+        if (query) {
+            filter.$or = [
+                { title: { $regex: query, $options: "i" } },
+                { description: { $regex: query, $options: "i" } }
+            ];
+        }
+        
+        if (category) {
+            filter.category = category;
+        }
+        if (available) {
+            filter.available = available === "true";
+        }
     
-        const result = await Product.paginate({}, options);
+        const result = await Product.paginate(filter, options);
         console.log(result)
         res.render("view_products", {
-          products: result.docs,
-          currentPage: result.page,
-          totalPages: result.totalPages,
-          hasPrevPage: result.hasPrevPage,
-          hasNextPage: result.hasNextPage,
-          prevPage: result.prevPage,
-          nextPage: result.nextPage,
-        });
+            layout: "products", // layout personalizado
+            status: result.docs.length > 0? 1 : 0,
+            payload: result.docs,
+            page: result.page,
+            totalPages: result.totalPages,
+            hasPrevPage: result.hasPrevPage,
+            hasNextPage: result.hasNextPage,
+            prevPage: result.prevPage,
+            nextPage: result.nextPage,
+            limit : limit,
+            query: query,
+            sort: sort,
+            available: available
+          });
       } catch (err) {
         res.status(500).send(err.message);
       }
